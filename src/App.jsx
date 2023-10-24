eb407cc0073142252936f622ef818291edeeba90
kiransahu-dev <sahukiran98.ks@gmail.com>
2023-10-24 16:32:10 +0530

    created protected routs for admin

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,11 @@ import MyState from "./context/data/myState";
 import Login from "./pages/registration/Login";
 import Signup from "./pages/registration/Signup";
 import ProductInfo from "./pages/productinfo/ProductInfo";
+import AddProduct from "./pages/admin/pages/AddProduct";
+import UpdateProduct from "./pages/admin/pages/UpdateProduct";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import AllProducts from "./pages/allproducts/AllProducts";
 
 const App = () => {
   return (
@@ -16,18 +21,68 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/order" element={<Order />} />
+          <Route path="/allproducts" element={<AllProducts />} />
+          <Route
+            path="/order"
+            element={
+              <ProtetctedRoute>
+                <Order />
+              </ProtetctedRoute>
+            }
+          />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoutesForAdmin>
+                <Dashboard />
+              </ProtectedRoutesForAdmin>
+            }
+          />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/productinfo/:id" element={<ProductInfo />} />
+          <Route
+            path="/addproduct"
+            element={
+              <ProtectedRoutesForAdmin>
+                <AddProduct />
+              </ProtectedRoutesForAdmin>
+            }
+          />
+          <Route
+            path="/updateproduct"
+            element={
+              <ProtectedRoutesForAdmin>
+                <UpdateProduct />
+              </ProtectedRoutesForAdmin>
+            }
+          />
           <Route path="/cart" element={<Cart />} />
           <Route path="/*" element={<PageNotFound />} />
         </Routes>
+        <ToastContainer />
       </Router>
     </MyState>
   );
 };
 
 export default App;
+
+export const ProtetctedRoute = ({ children }) => {
+  if (localStorage.getItem("user")) {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+};
+
+export const ProtectedRoutesForAdmin = ({ children }) => {
+  const admin = JSON.parse(localStorage.getItem("user"));
+  console.log(admin.user.email);
+  if (admin.user.email === "kiransahu@gmail.com") {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+};
